ef19a8e89141feb8296ec2c21c1a66489366ddfc
Kimserey Lam <kimserey.lam@gmail.com>
2019-08-31 18:08:37 +0100

    initial setup

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  heroes = [
-    {
-      id: 1,
-      name: 'Superman'
-    },
-    {
-      id: 2,
-      name: 'Batman'
-    }
-  ];
+  // heroes = [
+  //   {
+  //     id: 1,
+  //     name: 'Superman'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Batman'
+  //   }
+  // ];
+
+  // getHeroes(): Observable<{ id: number, name: string }[]> {
+  //   return of(this.heroes);
+  // }
+
+  // getHero(id: number): Observable<{ id: number, name: string }> {
+  //   return of(this.heroes.find(h => h.id === id));
+  // }
 
   getHeroes(): Observable<{ id: number, name: string }[]> {
-    return of(this.heroes);
+    return this.http.get<{ id: number, name: string }[]>('http://localhost:5000/heroes');
   }
 
   getHero(id: number): Observable<{ id: number, name: string }> {
-    return of(this.heroes.find(h => h.id === id));
+    return this.http.get<{ id: number, name: string }>(`http://localhost:5000/heroes/${id}`);
   }
+
+  constructor(private http: HttpClient) {}
 }
